cdc6bab147934e18611e9a7460fa2ec1e2722a54
Bruno Torres <brunotorresreal@outlook.com.br>
2022-09-28 10:05:37 -0300

    second commit

--- src/api.ts
+++ src/api.ts
@@ -4,7 +4,7 @@ const BASE_URL = `https://api.github.com/users/Brunotorresdev/repos`;
 
 export const api = {
     getListRepos: async () => {
-        let response = await axios.get(BASE_URL);
+        const response = await axios.get(BASE_URL);
         return response;
     },
 };
